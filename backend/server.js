5309f9b53e3fc27a385d912ac6e12179c3d34094
shrikantsahu2004 <sahu.sr@somaiya.edu>
2021-12-30 02:56:47 +0530

    feat: Backend: Send messages(1-1 and group)

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db");
 const { chats } = require("./data/data");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
@@ -18,6 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
